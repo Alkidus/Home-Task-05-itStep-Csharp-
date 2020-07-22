4374bd76133f63abe781be0af8a0810bd49777b0
Alkidus <68506257+Alkidus@users.noreply.github.com>
2020-07-22 14:37:18 +0300

    Add files via upload

---
+++ Home Task 5/Task 01/Program.cs
@@ -0,0 +1,74 @@
+﻿/*Реализовать класс    для хранения    комплексного числа.
+Выполнить    в нем    перегрузку всехнеобходимых операторов для успешной компиляции 
+следующего фрагмента кода:
+Complex z = newComplex(1, 1);
+Complex z1; z1 = z - (z* z * z - 1) / (3 * z * z);  
+Console.WriteLine("z1 =  {0}", z1);*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task_01
+{
+    class Complex
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+        public Complex() { }
+        public Complex(double re, double im)
+        {
+            Re = re;
+            Im = im;
+        }
+        public override string ToString()
+        {
+            return $"{Re: 0.00} + {Im: 0.0}i";
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            return new Complex { Re = c1.Re + c2.Re, Im = c1.Im + c2.Im };
+        }
+        public static Complex operator -(Complex c1, Complex c2)
+        {
+            return new Complex { Re = c1.Re - c2.Re, Im = c1.Im - c2.Im };
+        }
+        public static Complex operator -(Complex c1, int c2)
+        {
+            return new Complex { Re = c1.Re - c2, Im = c1.Im };
+        }
+        public static Complex operator *(Complex c1, Complex c2)
+        {
+            //(a+bi)(c+di) = (ac-bd)+(bc + ad)i
+            //a = c1.Re; b = c1.Im; c = c2.Re; d = c2.Im;
+            return new Complex { Re = (c1.Re * c2.Re - c1.Im * c2.Im), Im = (c1.Im * c2.Re + c1.Re * c2.Im) };
+        }
+        public static Complex operator *(int c1, Complex c2)
+        {
+            return new Complex { Re = c1 * c2.Re, Im = c1 * c2.Im };
+        }
+        public static Complex operator /(Complex c1, Complex c2)
+        {
+            //Re = (ac+bd)/(cc+dd); Im = (bc-ad)/(cc+dd)
+            double a = c1.Re; double b = c1.Im; double c = c2.Re; double d = c2.Im;
+            return new Complex { Re = (a * c + b * d) / (c * c + d * d), Im = (b * c - a * d) / (c * c + d * d) };
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForegroundColor = ConsoleColor.Green;
+            Complex z = new Complex(1, 1);
+            Complex z1;
+            z1 = z - (z * z * z - 1) / (3 * z * z);
+            WriteLine("z = {0}", z);
+            WriteLine("z1 = {0}", z1);
+            ReadKey();
+        }
+    }
+}
---
+++ Home Task 5/Task 02/Program.cs
@@ -0,0 +1,140 @@
+﻿/*Разработать класс Fraction, представляющий простую дробь.
+в классе  предусмотреть два  поля:  числитель и знаменатель дроби.
+Выполнить перегрузку следу-ющих операторов: +,-,*,/,==,!=,<,>, true и false .
+Арифметические действия и сравнение выполняется в соответствии с правилами работы с дробями.
+Оператор true возвращает true если дробь правильная(числитель меньше знаменателя),
+оператор false возвращает true если дробь неправильная(числитель больше знаменателя) .
+Выполнить перегрузку операторов, необходимых для успешной компиляции следующего фрагмента кода:
+Fraction f = newFraction(3, 4); 
+int a = 10; 
+Fraction f1 = f * a; 
+Fraction f2 = a * f; 
+double d = 1.5; 
+Fraction f3 = f + d;*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Task_02
+{
+    class Fraction
+    {
+        public int ches { get; set; }
+        public int znam { get; set; }
+        public Fraction() { }
+        public Fraction(int ches, int znam)
+        {
+            this.ches = ches;
+            this.znam = znam;
+        }
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            return new Fraction { ches = f1.ches * f2.znam + f2.ches * f1.znam, znam = f1.znam * f2.znam };
+        }
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            return new Fraction { ches = f1.ches * f2.znam - f2.ches * f1.znam, znam = f1.znam * f2.znam };
+        }
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            return new Fraction { ches = f1.ches * f2.ches, znam = f1.znam * f2.znam };
+        }
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            return new Fraction { ches = f1.ches * f2.znam, znam = f1.znam * f2.ches };
+        }
+        public override bool Equals(object obj)
+        {
+            return this.ToString() == obj.ToString();
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+        public static bool operator ==(Fraction f1, Fraction f2)
+        {
+            return f1.Equals(f2);
+        }
+        public static bool operator !=(Fraction f1, Fraction f2)
+        {
+            return !(f1 == f2);
+        }
+        public static bool operator >(Fraction f1, Fraction f2)
+        {
+            if (f1.ches * f2.znam > f1.znam * f2.ches)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <(Fraction f1, Fraction f2)
+        {
+            if (f1.ches * f2.znam < f1.znam * f2.ches)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator true(Fraction f1)
+        {
+            if (f1.ches < f1.znam)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator false(Fraction f1)
+        {
+            if (f1.ches > f1.znam)
+                return true;
+            else
+                return false;
+        }
+
+        //
+        //
+        //
+        //
+        public static Fraction operator *(Fraction f1, int f2)
+        {
+            return new Fraction { ches = f1.ches * f2, znam = f1.znam };
+        }
+        public static Fraction operator *(int f2, Fraction f1)
+        {
+            return new Fraction { ches = f1.ches * f2, znam = f1.znam };
+        }
+        public static Fraction operator +(Fraction f1, double f2)
+        {
+            return new Fraction { ches = f1.ches * 10 + (int)(f1.znam * f2 * 10), znam = f1.znam * 10 };
+        }
+        public override string ToString()
+        {
+            return $"{ches}/{znam}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction f = new Fraction(3, 4);
+            int a = 10;
+            Fraction f1 = f * a;
+            Fraction f2 = a * f;
+            double d = 1.5;
+            Fraction f3 = f + d;
+            WriteLine($"f1 = {f1}");
+            WriteLine($"f2 = {f2}");
+            WriteLine($"f3 = {f3}");
+            WriteLine($"f1 == f2: {f1 == f2}");
+            WriteLine($"f1 != f2: {f1 != f2}");
+            WriteLine($"f1 > f3: {f1 > f3}");
+            WriteLine($"f1 < f3: {f1 < f3}");
+            if (f1)
+                WriteLine($"{f1} - Дробь правильная");
+            else
+                WriteLine($"{f1} - Дробь неправильная");
+            ReadKey();
+        }
+    }
+}
